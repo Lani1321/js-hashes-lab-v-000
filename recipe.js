39612d88522482389622ff145c2343ee4e616833
Lani1321 <lani.berry13@gmail.com>
2016-11-10 17:46:34 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+var recipe = {ingredient: "amount"}
+
+function addIngredient(recipe, ingredientName, amounts) {
+  recipe[ingredientName] = amounts;
+  return recipe
+}
+
+function removeIngredient(recipe, ingredientName, amounts) {
+  delete recipe[ingredientName];
+  return recipe
+}
+
+function updateIngredient(recipe, ingredientName, amounts) {
+  recipe[ingredientName] = amounts;
+  return recipe
+}
+
+function readRecipe(recipe){
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
